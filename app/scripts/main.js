69cab81f393a5a4a12303cdd141adff25bb7b43b
agent1red <darklen14@gmail.com>
2017-02-06 00:36:17 -0800

    update files

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,23 +1,23 @@
 console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
 
-$(document).ready(function () {
+//$(document).ready(function () {
 
-    $(function () {
-        $("#hellowoldtype").typed({
-            strings: ["Hello World!!!", "Week 5 Assignment is complete!!!", "This is my hello world mini app.", "It displays Hello World and then loops."],
-            typeSpeed: 75,
-            backDelay: 500,
-            loop: true,
-            loopCount: false,
-            callback: function () { foo(); },
-            resetCallback: function () { newTyped(); }
-        });
-        $(".reset").click(function () {
-            $("#hellowoldtype").typed('reset');
-        });
-    });
-    function newTyped() { /* A new typed object */ }
-    function foo() { console.log("loop back"); }
+//    $(function () {
+//        $("#hellowoldtype").typed({
+//            strings: ["Hello World!!!", "Week 5 Assignment is complete!!!", "This is my hello world mini app.", "It displays Hello World and then loops."],
+//            typeSpeed: 75,
+//            backDelay: 500,
+//            loop: true,
+//            loopCount: false,
+//            callback: function () { foo(); },
+//            resetCallback: function () { newTyped(); }
+//        });
+//        $(".reset").click(function () {
+//            $("#hellowoldtype").typed('reset');
+//        });
+//    });
+//    function newTyped() { /* A new typed object */ }
+//    function foo() { console.log("loop back"); }
 
-})
+//})
 
